c07c850199df581b9073b00567791bbe0e108d92
jzv795 <viktoria.norekyan@capitalone.com>
2017-08-22 13:46:41 -0400

    Can swap pieces

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 (function() {
 
-  var populateCell = function(i,j) {
-    var randNum = Math.floor(Math.random() * 6) + 1;
-    console.log(randNum);
+  var populateBoard = function() {
 
-    switch(randNum) {
-      case 1:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./blue-diamond.jpeg)');
-        break;
-      case 2:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./green-diamond.jpg)');
-        break;
-      case 3:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./pink-sapphire.jpg)');
-        break;
-      case 4:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./blue-gem.jpg)');
-        break;
-      case 5:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./rubellite-tourmaline.jpg)');
-        break;
-      case 6:
-        $('.row-'+i+'-column-'+j).css('background-image', 'url(./pink-tourmaline.jpg)');
-        break;
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        var randNum = Math.floor(Math.random() * 6) + 1;
+
+        switch(randNum) {
+          case 1:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/blue-diamond.jpeg)');
+            break;
+          case 2:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/green-diamond.jpg)');
+            break;
+          case 3:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/pink-sapphire.jpg)');
+            break;
+          case 4:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/blue-gem.jpg)');
+            break;
+          case 5:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/rubellite-tourmaline.jpg)');
+            break;
+          case 6:
+            $('.row-'+i+'-column-'+j).css('background-image', 'url(./images/pink-tourmaline.jpg)');
+            break;
+        }
+      }
     }
   }
 
@@ -31,13 +35,38 @@
       var row = $('<tr>').attr('id', 'row-'+i);
       $('#gameboard')[0].append(row[0]);
       for (let j = 0; j < 9; j++) {
-        var column = $('<td>').addClass('row-'+i+'-column-'+j);
-        $('#row-'+i)[0].append(column[0]);
-        populateCell(i,j);
+        var cell = $('<td>').addClass('row-'+i+'-column-'+j);
+        var clicked = [];
+        cell.on('click', function() {
+          console.log($(this).attr('class'));
+          if (clicked.length === 0) {
+            clicked.push(this);
+            $(this).css('border','2px solid red');
+          } else if (clicked.length === 1) {
+            clicked.push(this);
+            $(this).css('border','2px solid red');
+            var first = clicked[0];
+            var second = clicked[1];
+
+            //swap the background images of the 2 clicked elements
+            var temp = first.style.backgroundImage;
+            first.style.backgroundImage = second.style.backgroundImage;
+            second.style.backgroundImage = temp;
+            first.style.border = '2px solid violet';
+            second.style.border = '2px solid violet';
+            console.log('2 clicked!');
+            clicked = [];
+          }
+        });
+        $('#row-'+i)[0].append(cell[0]);
       }
     }
     console.log($('#gameboard')[0]);
   }
 
+
+
   createBoard();
+  populateBoard();
+
 })();
