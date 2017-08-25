5a0db0616d5b0fc5394d55ded867bd547ce4a493
jzv795 <viktoria.norekyan@capitalone.com>
2017-08-25 10:35:53 -0400

    More granular progress bar

--- js/proceed.js
+++ js/proceed.js
@@ -95,12 +95,37 @@ var proceed = function(moves){
   switch(level){
     case 1:
       $('#moves-left').text('MOVES LEFT: ' + movesLeft);
-      if (score >= 100 && score < 200){
+
+      if (score >= 30 && score < 75){
+        $('.progress-bar-filling').css('height', (530-(0.1*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 75 && score < 100){
+        $('.progress-bar-filling').css('height', (530-(0.25*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 100 && score < 150){
         $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius', '5px');
       }
 
-      if (score >= 200 && score < 300){
-        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius','5px');
+      if (score >= 150 && score < 200){
+        $('.progress-bar-filling').css('height', (530-(0.5*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 200 && score < 225){
+        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 225 && score < 240){
+        $('.progress-bar-filling').css('height', (530-(0.75*530)) + 'px').css('border-radius','5px');
+      }
+
+      if (score >= 240 && score < 270){
+        $('.progress-bar-filling').css('height', (530-(0.8*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 270 && score < 300){
+        $('.progress-bar-filling').css('height', (530-(0.9*530)) + 'px').css('border-radius', '5px');
       }
 
       if(score >= 300 && moves <= 15) {
@@ -113,13 +138,38 @@ var proceed = function(moves){
       }
       break;
     case 2:
-      if (score >= 400 && score < 500){
-        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius','5px');
+      if (score >= 330 && score < 375){
+        $('.progress-bar-filling').css('height', (530-(0.1*530)) + 'px').css('border-radius', '5px');
       }
 
-      if (score >= 500 && score < 600){
-        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius','5px');
+      if (score >= 375 && score < 400){
+        $('.progress-bar-filling').css('height', (530-(0.25*530)) + 'px').css('border-radius', '5px');
       }
+
+      if (score >= 400 && score < 450){
+        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 450 && score < 500){
+        $('.progress-bar-filling').css('height', (530-(0.5*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 500 && score < 525){
+        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 525 && score < 540){
+        $('.progress-bar-filling').css('height', (530-(0.75*530)) + 'px').css('border-radius','5px');
+      }
+
+      if (score >= 540 && score < 570){
+        $('.progress-bar-filling').css('height', (530-(0.8*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 570 && score < 600){
+        $('.progress-bar-filling').css('height', (530-(0.9*530)) + 'px').css('border-radius', '5px');
+      }
+
       if(score >= 600 && moves <= 20) {
         level = 3;
         movesLeft = (25 - moves);
@@ -130,12 +180,36 @@ var proceed = function(moves){
       }
     break;
     case 3:
-      if (score >= 700 && score < 800){
-        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius','5px');
+      if (score >= 630 && score < 675){
+        $('.progress-bar-filling').css('height', (530-(0.1*530)) + 'px').css('border-radius', '5px');
       }
 
-      if (score >= 800 && score < 900){
-        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius','5px');
+      if (score >= 675 && score < 700){
+        $('.progress-bar-filling').css('height', (530-(0.25*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 700 && score < 750){
+        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 750 && score < 800){
+        $('.progress-bar-filling').css('height', (530-(0.5*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 800 && score < 825){
+        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 825 && score < 840){
+        $('.progress-bar-filling').css('height', (530-(0.75*530)) + 'px').css('border-radius','5px');
+      }
+
+      if (score >= 840 && score < 870){
+        $('.progress-bar-filling').css('height', (530-(0.8*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 870 && score < 900){
+        $('.progress-bar-filling').css('height', (530-(0.9*530)) + 'px').css('border-radius', '5px');
       }
 
       if(score >= 900 && moves <= 25) {
@@ -148,13 +222,26 @@ var proceed = function(moves){
       }
       break;
     case 4:
-      if (score >= 1000 && score < 1200){
-        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius','5px');
+      if (score >= 950 && score < 1000){
+        $('.progress-bar-filling').css('height', (530-(0.1*530)) + 'px').css('border-radius', '5px');
       }
 
-      if (score >= 1200 && score < 1400){
-        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius','5px');
+      if (score >= 1000 && score < 1100){
+        $('.progress-bar-filling').css('height', (530-(0.2*530)) + 'px').css('border-radius', '5px');
       }
+
+      if (score >= 1100 && score < 1200){
+        $('.progress-bar-filling').css('height', (530-(0.4*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1200 && score < 1300){
+        $('.progress-bar-filling').css('height', (530-(0.6*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1300 && score < 1400){
+        $('.progress-bar-filling').css('height', (530-(0.8*530)) + 'px').css('border-radius', '5px');
+      }
+
       if(score >= 1400 && moves <= 35) {
         level = 5;
         movesLeft = (40 - moves);
@@ -165,13 +252,38 @@ var proceed = function(moves){
       }
       break;
     case 5:
-      if (score >= 1500 && score < 1600){
-        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius','5px');
+      if (score >= 1430 && score < 1475){
+        $('.progress-bar-filling').css('height', (530-(0.1*530)) + 'px').css('border-radius', '5px');
       }
 
-      if (score >= 1600 && score < 1700){
-        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius','5px');
+      if (score >= 1475 && score < 1500){
+        $('.progress-bar-filling').css('height', (530-(0.25*530)) + 'px').css('border-radius', '5px');
       }
+
+      if (score >= 1500 && score < 1550){
+        $('.progress-bar-filling').css('height', (530-(0.333*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1550 && score < 1600){
+        $('.progress-bar-filling').css('height', (530-(0.5*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1600 && score < 1625){
+        $('.progress-bar-filling').css('height', (530-(0.66*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1625 && score < 1640){
+        $('.progress-bar-filling').css('height', (530-(0.75*530)) + 'px').css('border-radius','5px');
+      }
+
+      if (score >= 1640 && score < 1670){
+        $('.progress-bar-filling').css('height', (530-(0.8*530)) + 'px').css('border-radius', '5px');
+      }
+
+      if (score >= 1670 && score < 1700){
+        $('.progress-bar-filling').css('height', (530-(0.9*530)) + 'px').css('border-radius', '5px');
+      }
+
       if(score >= 1700 && moves <= 40) {
         winGame();
       } else if (moves === 40 && score < 1700){
